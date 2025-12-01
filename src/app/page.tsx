b01fb35ba41bb0f9f408566259b259ddd3da1107
shibili.P <shibiliwillowy@gmail.com>
2025-12-01 12:19:11 +0530

    home page foders structured properly and home page completed may be need future correction and optimization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
-import Header from "../app/components/common/Header";
-import Footer from "../app/components/common/Footer";
-import Hero from "../app/components/hero/Hero";
-import AboutSection from "../app/components/AboutSection";
-import ServicesSection from "../app/components/services/ServicesSection";
-import CoursesSection from "./components/courses/CoursesSection";
-import WhyUsSection from "./components/WhyUsSection";
-import TestimonialSection from "./components/TestimonialSection";
-import FAQSection from "./components/FAQSection";
-import LookingForSection from "./components/LookingForSection"
+import Header from "../components/common/Header";
+import Footer from "../components/common/Footer";
+import Hero from "../components/Home/Hero";
+import AboutSection from "../components/Home/AboutSection";
+import ServicesSection from "../components/Home/ServicesSection";
+import CoursesSection from "../components/Home/CoursesSection";
+import WhyUsSection from "../components/Home/WhyUsSection";
+import TestimonialSection from "../components/Home/TestimonialSection";
+import FAQSection from "../components/Home/FAQSection";
+import LookingForSection from "../components/Home/LookingForSection"
 
 export default function Home() {
   return (
